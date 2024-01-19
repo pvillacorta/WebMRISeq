5ca4932419158170a6d75a3c146b24dd00e0af7a
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2024-01-19 13:24:50 +0100

    Resize QQuickView

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     
     QQuickView view;
-    view.setSource(QUrl::fromLocalFile("../qml/Main.qml"));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    const QUrl url(u"qrc:/WebMRISequenceEditor/qml/Main.qml"_qs);
+    view.setSource(url);
     view.show();
     
 
